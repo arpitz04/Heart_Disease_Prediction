d7f1a070f0af99e8658be06ec6fcb04031d9b9e6
arpitz04 <arpitvarshney04@gmail.com>
2025-06-16 10:02:06 +0530

    Moved files to repo root and removed unused files

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-    fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl (1 = true; 0 = false)')
-    restecg = st.text_input('Resting Electrocardiographic Results (0-2)')
-    thalachh = st.text_input('Maximum Heart Rate Achieved')
